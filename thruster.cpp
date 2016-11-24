6274395cb9a861404223cb6ce7a186a727915113
shsssc <1005308775@qq.com>
2016-11-24 14:33:30 -0800

    Serial
    RT

--- thruster.cpp
+++ thruster.cpp
@@ -1,14 +1,20 @@
 #include "thruster.h"
+#include "Arduino.h"
+extern int lights[];
 int getThrusterServoMs(unsigned char in_data)
 {
-	int speed = (unsigned char)(in_data << 1) >> 1;
-	speed -= 64;
-	speed *= 7;
-	speed += 1500;
-	return speed;
+	int speed1 = (unsigned char)(in_data << 1) >> 1;
+	speed1 -= 64;
+	speed1 *= 7;
+	speed1 += 1500;
+	return speed1;
 }
-void sendThrusterSpeed(unsigned char selector, short speed)
+void sendThrusterSpeed(unsigned char selector, short speed1)
 {
 	//
+        speed1-=1500;
+        speed1/=7;
+        speed1+=64;
+        analogWrite(lights[selector],speed1);
 	return;
 }
